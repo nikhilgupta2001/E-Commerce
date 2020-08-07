3486779b213a52715643dd5249cfe97c2dd64b69
Your Name <you@example.com>
2020-08-07 10:16:21 +0530

    Categoriesupdate

--- client/src/components/layout/categories/categories.js
+++ client/src/components/layout/categories/categories.js
@@ -12,17 +12,21 @@ const categories=(props)=>
     <Col><img src="./images/Footwear.webp" className={classes.img} alt="img"></img></Col>
      <Col><img src="./images/Dresses.webp" className={classes.img}></img></Col>
      <Col><img src="./images/Dresses2.webp" className={classes.img}></img></Col>
+     <Col><img src="./images/Dresses2.webp" className={classes.img}></img></Col>
     </Row>
     <Row className={classes.col}>
      <Col><img src="./images/Kurta.webp" className={classes.img}></img></Col>
      <Col><img src="./images/jeans.webp" className={classes.img}></img></Col>
+     <Col><img src="./images/Sarees.webp" className={classes.img}></img></Col>
      <Col><img src="./images/Sarees.webp" className={classes.img}></img></Col>
     </Row>
     <Row className={classes.col}>
     <Col><img src="./images/Handbags.webp" className={classes.img}></img></Col>
      <Col><img src="./images/T-shirt.webp" className={classes.img}></img></Col>
      <Col><img src="./images/Tops.webp" className={classes.img}></img></Col>
+     <Col><img src="./images/Tops.webp" className={classes.img}></img></Col>
     </Row>
+    
    </Container> 
         </div>
 );
